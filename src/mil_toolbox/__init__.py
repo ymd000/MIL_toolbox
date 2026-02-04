1285c2823c0d89433a3d5d37b3b5361e08cb862c
ymd000 <a.yamada18318@gmail.com>
2026-02-04 11:27:33 +0900

    feat:

---
+++ src/mil_toolbox/__init__.py
@@ -0,0 +1,31 @@
+"""MIL Toolbox - Multiple Instance Learning training and visualization toolkit."""
+
+from .data import FoldManager, FoldInfo, WSIDataset, DummyWSIDataset, MILDataModule
+from .models import MILModel
+from .train import CrossValidationTrainer
+from .inference import (
+    AttentionAggregator,
+    MILPredictor,
+    PreviewAttention,
+    SlideEmbeddingCalculator,
+)
+
+__version__ = "0.0.1"
+
+__all__ = [
+    # Data
+    "FoldManager",
+    "FoldInfo",
+    "WSIDataset",
+    "DummyWSIDataset",
+    "MILDataModule",
+    # Models
+    "MILModel",
+    # Training
+    "CrossValidationTrainer",
+    # Inference
+    "AttentionAggregator",
+    "MILPredictor",
+    "PreviewAttention",
+    "SlideEmbeddingCalculator",
+]
---
+++ src/mil_toolbox/utils/__init__.py
@@ -0,0 +1,5 @@
+"""Utility functions for MIL Toolbox."""
+
+from .plot_training_log import plot_training_metrics, load_metrics
+
+__all__ = ["plot_training_metrics", "load_metrics"]
